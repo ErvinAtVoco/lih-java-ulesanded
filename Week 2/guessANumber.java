ace9c1c0684d78fd530e9b5ed3f4de6c6fcf31d7
ErvinAtVoco <122527751+ErvinAtVoco@users.noreply.github.com>
2023-10-19 12:36:04 +0300

    Finished Week 2 exercises

--- Week 2/guessANumber.java
+++ Week 2/guessANumber.java
@@ -3,10 +3,28 @@ import java.util.Scanner;
 public class main {
 	public static void main (String[] args){
 		Scanner reader = new Scanner(System.in);
-		number = 
+		int guess;
+		int number = 39;
+		int attempts = 0;
 		while(true){
+			System.out.print("Guess a number: ");
+			guess = Integer.parseInt(reader.nextLine());
+			if (guess == number) {
+				System.out.println("You got it!");
+				attempts += 1;
+				System.out.println("It took you " + attempts + " attempts!");
+				break;
+			};
 			
+			if (guess > number) {
+				System.out.println("The number is lesser than " + guess);
+				attempts += 1;
+			} else {
+				System.out.println("The number is greater than " + guess);
+				attempts += 1;
+			};
+			
+			System.out.println("Attempts: " + attempts);
 		}; 
-
 	};
 };
